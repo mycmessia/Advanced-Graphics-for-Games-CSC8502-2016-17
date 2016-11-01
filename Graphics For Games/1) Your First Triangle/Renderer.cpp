cca7bb62a721a4fbee1da75efedd2328061aecea
Yuchen Mei <mycmessia@gmail.com>
2016-11-01 15:20:55 +0000

    First commit

---
+++ Graphics For Games/1) Your First Triangle/Renderer.cpp
@@ -0,0 +1,36 @@
+#include "Renderer.h"
+
+Renderer::Renderer(Window &parent) : OGLRenderer(parent) {
+	triangle = Mesh::GenerateTriangle ();
+
+	currentShader = new Shader (SHADERDIR"basicVertex.glsl", SHADERDIR"colourFragment.glsl");
+
+	if (!currentShader->LinkProgram()) {
+		return;
+	}
+
+	init = true;
+}
+
+Renderer::~Renderer(void) 
+{
+	delete triangle;
+}
+
+void Renderer::RenderScene() 
+{
+	glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
+
+	glClear (GL_COLOR_BUFFER_BIT);
+
+	glUseProgram (currentShader->GetProgram());
+
+	triangle->ChangeColor ();
+
+	triangle->Draw ();
+
+	glUseProgram (0);
+
+	SwapBuffers ();
+}
+
