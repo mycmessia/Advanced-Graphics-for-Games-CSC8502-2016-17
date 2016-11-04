e929462df03615d4debc3aa6db2a838a0f1ece34
Yuchen Mei <mycmessia@gmail.com>
2016-11-04 11:13:56 +0000

    Change Visual Studio 2015 settings and export them

--- Graphics For Games/3) Texturing/tutorial3.cpp
+++ Graphics For Games/3) Texturing/tutorial3.cpp
@@ -3,40 +3,46 @@
 #include "../../nclGL/window.h"
 #include "Renderer.h"
 
-int main () 
+int main ()
 {
 	Window w ("Texturing!", 800, 600, false);	 //This is all boring win32 window creation stuff!
-	if (!w.HasInitialised ()) {
+	if (!w.HasInitialised ())
+	{
 		return -1;
 	}
-	
+
 	Renderer renderer (w);	//This handles all the boring OGL 3.2 initialisation stuff, and sets up our tutorial!
-	if (!renderer.HasInitialised ()) {
+	if (!renderer.HasInitialised ())
+	{
 		return -1;
 	}
 
 	float rotate = 0.0f;
 	while (w.UpdateWindow () && !Window::GetKeyboard ()->KeyDown (KEYBOARD_ESCAPE))
 	{
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_LEFT) ) {
+		if (Window::GetKeyboard ()->KeyDown (KEYBOARD_LEFT))
+		{
 			--rotate;
-			renderer.UpdateTextureMatrix(rotate);
+			renderer.UpdateTextureMatrix (rotate);
 		}
 
-		if (Window::GetKeyboard()->KeyDown(KEYBOARD_RIGHT) ) {
+		if (Window::GetKeyboard ()->KeyDown (KEYBOARD_RIGHT))
+		{
 			++rotate;
-			renderer.UpdateTextureMatrix(rotate);
+			renderer.UpdateTextureMatrix (rotate);
 		}
 
-		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_1) ) {
-			renderer.ToggleFiltering();
+		if (Window::GetKeyboard ()->KeyTriggered (KEYBOARD_1))
+		{
+			renderer.ToggleFiltering ();
 		}
 
-		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_2) ) {
-			renderer.ToggleRepeating();
+		if (Window::GetKeyboard ()->KeyTriggered (KEYBOARD_2))
+		{
+			renderer.ToggleRepeating ();
 		}
 
-		renderer.RenderScene();
+		renderer.RenderScene ();
 	}
 
 	return 0;
